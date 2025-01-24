db56a5bcbae554299094abeffd83c701d41dbe68
Umbrases <zhenua27@gmail.com>
2025-01-24 11:55:23 +0500

    finish update

--- index.php
+++ index.php
@@ -22,32 +22,45 @@ function getFolderAll($folderId, $start = 0)
 
 function comparison($contentArchive, $contentFolder, $folderId)
 {
-    foreach ($contentArchive as $keyArchive => $valueArchive) {
-        foreach ($contentFolder as $keyFolder => $valueFolder) {
-            if ($valueArchive['NAME'] == $valueFolder['NAME']) {
-                if ($valueArchive['TYPE'] == 'folder') {
-                    $contentArc = getFolderAll($valueArchive['ID']);
-                    $contentFol = getFolderAll($valueFolder['ID']);
-
-                    if (!empty($contentFol['result'])) {
-                        comparison($contentArc['result'], $contentFol['result'], $valueFolder['ID']);
+    foreach ($contentArchive as $keyArc => $valueArc) {
+        if (!empty($contentFolder)) {
+            foreach ($contentFolder as $keyFol => $valueFol) {
+                if ($valueArc['TYPE'] == 'folder') {
+                    if ($valueArc['NAME'] == $valueFol['NAME']) {
+                        setFolderChildren($valueArc['ID'], $valueFol['ID']);
                     } else {
-                        foreach ($contentArc['result'] as $key => $value) {
-                            if ($value['TYPE'] == 'folder') {
-                                setFolder($valueFolder['ID'], $value['NAME']);
-                            }
+                        $folder = getFolder($folderId, $valueArc['NAME']);
+
+                        if (empty($folder)) {
+                            $folderAdd = setFolder($folderId, $valueArc['NAME']);
+
+                            setFolderChildren($valueArc['ID'], $folderAdd['result']['ID']);
                         }
                     }
+                } else {
+                    if ($valueArc['NAME'] != $valueFol['NAME']) {
+                        setFile($valueArc['ID'], $folderId);
+                    }
                 }
+            }
+        } else {
+            if ($valueArc['TYPE'] == 'folder') {
+                $folderAdd = setFolder($folderId, $valueArc['NAME']);
+
+                setFolderChildren($valueArc['ID'], $folderAdd['result']['ID']);
             } else {
-                if ($valueArchive['TYPE'] == 'folder') {
-                    setFolder($folderId, $valueArchive['NAME']);
-                } elseif ($valueArchive['TYPE'] == 'file') {
-                    setFile($valueArchive['ID'], $folderId);
-                }
+                setFile($valueArc['ID'], $folderId);
             }
         }
     }
+}
+
+function setFolderChildren($ArcId, $folderId)
+{
+    $contentArcChildren = getFolderAll($ArcId);
+    $contentFolChildren = getFolderAll($folderId);
+
+    comparison($contentArcChildren['result'], $contentFolChildren['result'], $folderId);
 }
 
 function setFolder($folderId, $folderName)
@@ -67,32 +80,38 @@ function setFile($fileId, $folderId)
     ]);
 }
 
-function preprint($data){
-    echo '<pre>';
-    print_r($data);
-    echo '</pre>';
-}
-
 //926575 - id папки клиенты
 //403 - id папки Kliyenty. Bankrotstvo
 
-$result = getFolderAll(926575);
+function start($start = 0)
+{
+    $log = "\n------------------------\n";
+    $log .= print_r($start, 1);
+    file_put_contents(getcwd() . '/hook.log', $log, FILE_APPEND);
 
-if(!empty($result['next'])) $next = $result['next'];
+    $result = getFolderAll(926575, $start);
 
-foreach ($result['result'] as $key => $value) {
-    $folderArr = getFolderAll($value['ID']);
+    foreach ($result['result'] as $key => $value) {
+        $folderArr = getFolderAll($value['ID']);
 
-    foreach ($folderArr['result'] as $folderKey => $folderValue) {
-        $folder = getFolder(403, $folderValue['NAME']);
+        foreach ($folderArr['result'] as $folderKey => $folderValue) {
 
-        $contentFolder = getFolderAll($folder['ID']);
-        $contentArchive = getFolderAll($folderValue['ID']);
 
-        if (!empty($contentFolder)){
+            $folder = getFolder(403, $folderValue['NAME']);
+
+            $contentFolder = getFolderAll($folder['ID']);
+            $contentArchive = getFolderAll($folderValue['ID']);
+
+            if (empty($contentFolder)) {
+                $folderAdd = setFolder($folder['ID'], $contentArchive['NAME']);
+                $contentFolder = getFolderAll($folderAdd['result']['id']);
+            }
+
             comparison($contentArchive['result'], $contentFolder['result'], $folder['ID']);
-        }else{
-            setFolder($folder['ID'], $contentArchive['NAME']);
         }
     }
+
+    if(!empty($result['next'])) start($result['next']);
 }
+
+start();
